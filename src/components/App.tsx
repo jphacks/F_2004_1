bb99548b8cbc6c50d1791bbcd6c5a085624a35f1
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2020-11-07 00:08:06 +0900

    全ユーザーを取得し、それぞれの集中度のグラフを表示するようにした

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,9 @@
 import React, { FC, useEffect, useState } from 'react'
 import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
-import { ConcentrationValue } from '../types'
+import { ConcentrationValue, User } from '../types'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 import { formatToHMS } from '../utils'
-import { Box } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
 import LimitSlider from './LimitSlider'
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -20,79 +20,106 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 const App: FC = () => {
   const classes = useStyles()
+  const apiUrl = process.env.REACT_APP_API_URL
 
-  const [concentrationValues, setConcentrationValues] = useState<
-    ConcentrationValue[]
-  >()
   const [limit, setLimit] = useState<number>(100)
-  // const [userId, setUserId] = useState<number>()
+  const [users, setUsers] = useState<User[]>([])
+  const [concentrationValues, setConcentrationValues] = useState<
+    ConcentrationValue[][]
+  >([])
 
   useEffect(() => {
-    // const date = ""
-    const endpoint = `${process.env.REACT_APP_API_URL}/concentration_values/1?limit=${limit}`
-    console.log(endpoint)
+    fetch(`${apiUrl}/users`)
+      .then(userResponse => userResponse.json())
+      .then(userResponseJson => {
+        if (userResponseJson.status != 'success') {
+          const message = `Response status is not success\n status: ${userResponseJson.status}, message: ${userResponseJson.message}`
+          throw Error(message)
+        }
 
-    fetch(endpoint)
-      .then(response => response.json())
-      .then(responseJson => {
-        if (responseJson.status !== 'success')
-          throw Error('Response status is not success')
+        setUsers(userResponseJson.users)
 
-        const concentrationValues: ConcentrationValue[] = responseJson.concentration_values
-          .slice(0, limit)
-          .map((data: Record<string, never>) => {
-            const datetime = new Date(data.created_at)
-            const concentrationValue = data.concentration_value
-            const isSitting = data.is_sitting
+        userResponseJson.users.forEach((user: User, index: number) => {
+          fetch(`${apiUrl}/concentration_values/${user.id}?limit=${limit}`)
+            .then(concentrationValuesResponse =>
+              concentrationValuesResponse.json()
+            )
+            .then(concentrationValuesResponseJson => {
+              if (concentrationValuesResponseJson.status !== 'success') {
+                const message = `Response status is not success\n status: ${concentrationValuesResponseJson.status}, message: ${concentrationValuesResponseJson.message}`
+                throw Error(message)
+              }
 
-            return {
-              datetime: formatToHMS(datetime),
-              concentrationValue: concentrationValue,
-              isSitting: isSitting
-            }
-          })
+              const values: ConcentrationValue[] = concentrationValuesResponseJson.concentration_values
+                .map((value: Record<string, never>) => {
+                  const datetime = new Date(value.created_at)
+                  const concentrationValue = value.concentration_value
+                  const isSitting = value.is_sitting
 
-        setConcentrationValues(concentrationValues.reverse())
+                  return {
+                    datetime: formatToHMS(datetime),
+                    concentrationValue: concentrationValue,
+                    isSitting: isSitting
+                  }
+                })
+                .reverse()
+
+              setConcentrationValues(prevState => {
+                const newState = JSON.parse(JSON.stringify(prevState))
+                newState[index] = values
+
+                return newState
+              })
+            })
+        })
       })
       .catch(error => console.log(error))
-  }, [limit])
+  }, [apiUrl, limit])
+
+  const charts = (): JSX.Element[] => {
+    return users.map((user: User, index: number) => (
+      <Box key={index}>
+        <Typography>{user.name}</Typography>
+        <LineChart
+          width={1000}
+          height={500}
+          data={concentrationValues[index]}
+          className={classes.lineChart}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="datetime"
+            label={{
+              value: 'Datetime',
+              position: 'insideBottomRight'
+            }}
+            height={50}
+          />
+          <YAxis
+            type="number"
+            domain={[1, 10]}
+            label={{
+              value: 'How concentrated',
+              angle: -90
+            }}
+            width={100}
+          />
+          <Tooltip formatter={value => [value, 'How concentrated']} />
+          {/*<Legend formatter={() => 'How concentrated'} iconSize={20} />*/}
+          <Line
+            type="monotone"
+            dataKey="concentrationValue"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </Box>
+    ))
+  }
 
   return (
     <Box className={classes.root}>
-      <h1 className={classes.title}>How you concentrated</h1>
-      <LineChart
-        width={1000}
-        height={500}
-        data={concentrationValues}
-        className={classes.lineChart}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis
-          dataKey="datetime"
-          label={{
-            value: 'Datetime',
-            position: 'insideBottomRight'
-          }}
-          height={50}
-        />
-        <YAxis
-          type="number"
-          domain={[1, 10]}
-          label={{
-            value: 'How concentrated',
-            angle: -90
-          }}
-          width={100}
-        />
-        <Tooltip formatter={value => [value, 'How concentrated']} />
-        {/*<Legend formatter={() => 'How concentrated'} iconSize={20} />*/}
-        <Line
-          type="monotone"
-          dataKey="concentrationValue"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
+      {charts()}
       <LimitSlider limit={limit} setLimit={setLimit} />
     </Box>
   )
